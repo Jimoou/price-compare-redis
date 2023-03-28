fbcf864b08aee80f2a0167deb08e1cdcf9e6591d
Jiwoon <kfromh0136@Naver.com>
2023-03-28 23:52:18 +0900

    Implement Lowest Price Product Retrieval from Redis
    이 PR에서는 다음의 기능을 구현하였습니다:
    
    1. `getZsetValue` 주어진 key에 해당하는 상품 정보를 Redis에서 가져옵니다.
    2. `setNewProdcut` 새 상품을 Redis에 추가하고, 상품의 순위를 반환합니다.
    3. `setNewProductGroup` 새 상품 그룹을 Redis에 추가하고, 상품 그룹의 크기를 반환합니다.
    4. `setNewProductGrpToKeyword` 새 상품 그룹을 키워드에 추가하고, 상품 그룹의 순위를 반환합니다.
    5. `getLowestPriceProductByKeyword` 주어진 키워드에 해당하는 가장 저렴한 상품을 가져옵니다.
    
    LowestPriceService 클래스에 상기된 기능들을 구현하였고, LowestPriceController 클래스에서 해당 서비스를 사용하여 API를 제공합니다.
    
    변경 사항:
    - Vo 클래스 어노테이션 추가 :  Product, ProductGroup
    - LowestPriceService 클래스 생성
    - LowestPriceController 클래스 생성
    
    이 PR은 Redis를 사용하여 주어진 키워드에 해당하는 최저가 상품을 검색하고 관련 정보를 반환하는 기능을 추가합니다.

---
+++ src/main/java/com/example/pricecompare/Controller/LowestPriceController.java
@@ -0,0 +1,52 @@
+package com.example.pricecompare.Controller;
+
+import com.example.pricecompare.Service.LowestPriceService;
+import com.example.pricecompare.Vo.Keyword;
+import com.example.pricecompare.Vo.Product;
+import com.example.pricecompare.Vo.ProductGroup;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/")
+@AllArgsConstructor
+public class LowestPriceController {
+
+  private LowestPriceService lowestPriceService;
+
+  @Operation(description = "주어진 key에 해당하는 상품 정보를 Redis에서 가져옵니다.")
+  @GetMapping("/get-zset-value")
+  public Set GetZsetValue(String key) {
+    return lowestPriceService.getZsetValue(key);
+  }
+
+  @Operation(description = "새 상품을 Redis에 추가하고, 상품의 순위를 반환합니다")
+  @PutMapping("/product")
+  public int SetNewProduct(@RequestBody Product newProduct) {
+    return lowestPriceService.setNewProduct(newProduct);
+  }
+
+  @Operation(description = "새 상품 그룹을 Redis에 추가하고, 상품 그룹의 크기를 반환합니다.")
+  @PutMapping("/propduct-group")
+  public int SetNewProductGroup(@RequestBody ProductGroup newProduct) {
+    return lowestPriceService.setNewProductGroup(newProduct);
+  }
+
+  @Operation(description = "새 상품 그룹을 키워드에 추가하고, 상품 그룹의 순위를 반환합니다.")
+  @PutMapping("/product-group-to-keyword")
+  public int SetNewProductGrpToKeyword(String keyword, String prodGroupId, double score) {
+    return lowestPriceService.setNewProductGrpToKeyword(keyword, prodGroupId, score);
+  }
+
+  @Operation(description = "주어진 키워드에 해당하는 가장 저렴한 상품을 가져옵니다.")
+  @GetMapping("/prodcut-price/lowest")
+  public Keyword GetLowestPriceProductByKeyword(String keyword) {
+    return lowestPriceService.getLowestPriceProductByKeyword(keyword);
+  }
+}
