a823d19fad229b41e7a66651cf4164f3c5884c59
Jiwoon <kfromh0136@Naver.com>
2023-03-29 00:25:45 +0900

    [REFACTOR] Lowest Price Product
    이 PR에서는 다음의 기능을 수정했습니다:
    
    1. Swagger UI에서 보기 편하게, summary를 추가했습니다.

--- src/main/java/com/example/pricecompare/Controller/LowestPriceController.java
+++ src/main/java/com/example/pricecompare/Controller/LowestPriceController.java
@@ -20,31 +20,31 @@ public class LowestPriceController {
 
   private LowestPriceService lowestPriceService;
 
-  @Operation(description = "주어진 key에 해당하는 상품 정보를 Redis에서 가져옵니다.")
+  @Operation(summary = "key에 해당하는 상품 정보", description = "주어진 key에 해당하는 상품 정보를 Redis에서 가져옵니다.")
   @GetMapping("/get-zset-value")
   public Set GetZsetValue(String key) {
     return lowestPriceService.getZsetValue(key);
   }
 
-  @Operation(description = "새 상품을 Redis에 추가하고, 상품의 순위를 반환합니다")
-  @PutMapping("/product")
+  @Operation(summary = "새 상품 추가", description = "새 상품을 Redis에 추가하고, 상품의 순위를 반환합니다")
+  @PutMapping("/product-add")
   public int SetNewProduct(@RequestBody Product newProduct) {
     return lowestPriceService.setNewProduct(newProduct);
   }
 
-  @Operation(description = "새 상품 그룹을 Redis에 추가하고, 상품 그룹의 크기를 반환합니다.")
+  @Operation(summary = "새 상품 그룹 추가", description = "새 상품 그룹을 Redis에 추가하고, 상품 그룹의 크기를 반환합니다.")
   @PutMapping("/propduct-group")
   public int SetNewProductGroup(@RequestBody ProductGroup newProduct) {
     return lowestPriceService.setNewProductGroup(newProduct);
   }
 
-  @Operation(description = "새 상품 그룹을 키워드에 추가하고, 상품 그룹의 순위를 반환합니다.")
+  @Operation(summary = "새 상품 그룹을 키워드에 추가", description = "새 상품 그룹을 키워드에 추가하고, 상품 그룹의 순위를 반환합니다.")
   @PutMapping("/product-group-to-keyword")
   public int SetNewProductGrpToKeyword(String keyword, String prodGroupId, double score) {
     return lowestPriceService.setNewProductGrpToKeyword(keyword, prodGroupId, score);
   }
 
-  @Operation(description = "주어진 키워드에 해당하는 가장 저렴한 상품을 가져옵니다.")
+  @Operation(summary = "키워드에 해당하는 최저가 상품", description = "주어진 키워드에 해당하는 가장 저렴한 상품을 가져옵니다.")
   @GetMapping("/prodcut-price/lowest")
   public Keyword GetLowestPriceProductByKeyword(String keyword) {
     return lowestPriceService.getLowestPriceProductByKeyword(keyword);
